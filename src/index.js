0556a9541cc5cff8aa3ab00b1df03007841a5813
kamalogudah <mcpaul2058@gmail.com>
2019-11-26 16:58:26 +0300

    Extracting JSX to new Components

--- src/index.js
+++ src/index.js
@@ -24,24 +24,7 @@ const App = () => {
            </div>
        </div>
 
-       <div className="comment">
-           <a href="/" className="avatar">
-               <img alt="avatar" src={faker.image.avatar()}/>
-           </a>
-           <div className="content">
-               <a href="/" className="author">
-                   Paul
-               </a>
-               <div className="metadata">
-                   <span className="date">
-                       Today at 6:00 PM
-                   </span>
-               </div>
-               <div className="text">
-                   Nice Blog Post
-               </div>
-           </div>
-       </div>
+       
 
        <div className="comment">
            <a href="/" className="avatar">
